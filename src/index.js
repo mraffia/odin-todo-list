3bd91f6c66407d52edd790571aacc8d0b075a576
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-07-09 17:51:18 +0700

    Add add todo logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './style.css';
+import Todo from './todo.js';
 
+let currentList = [];
+
+function addTodo(title, description = null, dueDate = null, priority = 4) {
+    const newTodo = Todo(title, description, dueDate, priority);
+    currentList.push(newTodo);
+    return newTodo;
+}
+
+function currentPage() {
+    let addExample = addTodo('Bruh');
+    console.log(addExample.getTitle());
+    console.log(currentList);
+}
+
+currentPage();
