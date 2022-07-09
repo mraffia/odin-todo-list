74ff1265da833d35e34706246132ec7277192488
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-07-09 17:23:15 +0700

    Update Todo object

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import './style.css';
 
-const Todo = (title, description, dueDate, priority) => {
-    const getTitle = () => title;
-    const getDescription  = () => description;
-    const getDueDate = () => dueDate;
-    const getPriority = () => priority;
-}
--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,52 @@
-const Todo = (title, description, dueDate, priority) => {
-    const getTitle = () => title;
-    const getDescription  = () => description;
-    const getDueDate = () => dueDate;
-    const getPriority = () => priority;
+const Todo = (title, description = null, dueDate = null, priority = 4) => {
+    let todoTitle = title;
+    let todoDesc = description;
+    let todoDueDate = dueDate;
+    let todoPriority = priority;
+    let completeStatus = false;
+    
+    const getTitle = () => todoTitle;
+    const getDescription  = () => todoDesc;
+    const getDueDate = () => todoDueDate;
+    const getPriority = () => todoPriority;
+    const getStatus = () => completeStatus;
+
+    const setTitle = newTitle => {
+        todoTitle = newTitle;
+    }
+
+    const setDesc = newDesc => {
+        todoDesc = newDesc;
+    }
+
+    const setDueDate = newDueDate => {
+        todoDueDate = newDueDate;
+    }
+
+    const setPriority = newPriority => {
+        todoPriority = newPriority;
+    }
+
+    const toogleStatus = () => {
+        if (completeStatus === false) {
+            completeStatus = true;
+        } else {
+            completeStatus = false;
+        }
+    } 
+
+    return { 
+        getTitle, 
+        getDescription, 
+        getDueDate, 
+        getPriority,
+        getStatus,
+        setTitle,
+        setDesc,
+        setDueDate,
+        setPriority,
+        toogleStatus,
+    }
 }
 
 export default Todo;
