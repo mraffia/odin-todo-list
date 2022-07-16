97f7cc01c183b1ba521a031f699b78b8b9aac132
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-07-16 16:51:05 +0700

    Add path element variables to complement the svgs

--- src/page.js
+++ src/page.js
@@ -5,22 +5,28 @@ function generatePage(projectPage = 'inbox') {
     const container = document.createElement('div');
     const header = document.createElement('div');
     const headerLogoContainer = document.createElement('div');
-    const headerLogo = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const headerLogoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const headerLogoPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
     const headerLogoName = document.createElement('div');
     const profileContainer = document.createElement('div');
     const profilePic = document.createElement('img');
-    const addTodoGeneral = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const addTodoGeneralSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const addTodoGeneralPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
     const sidebar = document.createElement('div');
-    const sidebarMenuMainContainer = document.createElement('div');
+    const sidebarMenuMain= document.createElement('div');
     const sidebarMenu = document.createElement('div');
-    const inboxLogo = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    const todayLogo = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const inboxLogoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const inboxLogoPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    const todayLogoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const todayLogoPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
     const sidebarTitle = document.createElement('h3');
-    const sidebarMenuProjectsContainer = document.createElement('div');
-    const projectLogo = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const sidebarMenuProjects = document.createElement('div');
+    const projectLogoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const projectLogoPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
     const addProjectContainer = document.createElement('div');
-    const addProjectLogo = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const addProjectLogoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const addProjectLogoPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
     const addProjectName = document.createElement('div');
 
     const mainContent = document.createElement('div');
@@ -28,28 +34,31 @@ function generatePage(projectPage = 'inbox') {
     const todoContainer = document.createElement('div');
     const singleTodo = document.createElement('div');
     const addTodoContainer = document.createElement('div');
-    const addTodoLogo = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const addTodoLogoSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const addTodoLogoPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
     const addTodoName = document.createElement('div');
+
+    const footer = document.createElement('div');
 
     container.classList.add('container');
     header.classList.add('header');
     headerLogoContainer.classList.add('header-logo-container');
-    headerLogo.classList.add('header-logo');
+    headerLogoSvg.classList.add('header-logo');
     headerLogoName.classList.add('header-logo-name');
     profileContainer.classList.add('profile-container');
     profilePic.classList.add('profile-pic');
-    addTodoGeneral.classList.add('add-todo-general');
+    addTodoGeneralSvg.classList.add('add-todo-general');
 
     sidebar.classList.add('sidebar');
-    sidebarMenuMainContainer.classList.add('sidebar-menu-main-container');
-    sidebarMenuProjectsContainer.classList.add('sidebar-menu-projects-container');
+    sidebarMenuMain.classList.add('sidebar-menu-main');
+    sidebarMenuProjects.classList.add('sidebar-menu-projects');
     sidebarMenu.classList.add('sidebar-menu');
-    inboxLogo.classList.add('inbox-logo');
-    todayLogo.classList.add('today-logo');
+    inboxLogoSvg.classList.add('inbox-logo');
+    todayLogoSvg.classList.add('today-logo');
     sidebarTitle.classList.add('sidebar-title');
-    projectLogo.classList.add('project-logo');
+    projectLogoSvg.classList.add('project-logo');
     addProjectContainer.classList.add('add-project-container');
-    addProjectLogo.classList.add('add-project-logo');
+    addProjectLogoSvg.classList.add('add-project-logo');
     addProjectName.classList.add('add-project-name');
 
     mainContent.classList.add('main-content');
@@ -57,8 +66,10 @@ function generatePage(projectPage = 'inbox') {
     todoContainer.classList.add('todo-container');
     singleTodo.classList.add('todo');
     addTodoContainer.classList.add('add-todo-container');
-    addTodoLogo.classList.add('add-todo-logo');
+    addTodoLogoSvg.classList.add('add-todo-logo');
     addTodoName.classList.add('add-todo-name');
 
+    footer.classList.add('footer');
 
+    headerLogoName.textContent = "Odin Todo List";
 }
